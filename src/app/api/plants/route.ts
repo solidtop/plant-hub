7bc8a80804f13e0f3b645c6d70da584084a3cbd4
Axel Asp <solidtop.asp@gmail.com>
2023-12-17 12:22:51 +0100

    Add query param

--- src/app/api/plants/route.ts
+++ src/app/api/plants/route.ts
@@ -13,9 +13,10 @@ type Payload = {
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
+  const query = searchParams.get("query");
   const page = searchParams.get("page");
 
-  const apiUrl = `https://perenual.com/api/species-list?key=${process.env.API_KEY}&page=${page}`;
+  const apiUrl = `https://perenual.com/api/species-list?key=${process.env.API_KEY}&q=${query}&page=${page}`;
   const res = await fetch(apiUrl);
   const payload: Payload = await res.json();
 
