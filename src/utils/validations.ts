e31dee639346c140ffba9783e8386f97b2e6e1d2
Axel Asp <solidtop.asp@gmail.com>
2023-12-17 18:43:58 +0100

    Add register endpoint & registerFormSchema

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -20,3 +20,28 @@ export const loginFormSchema = z.object({
       message: `Password must contain at most ${PASSWORD_MAX_LENGTH} characters`,
     }),
 });
+
+export const registerFormSchema = z
+  .object({
+    username: z
+      .string()
+      .min(USERNAME_MIN_LENGTH, {
+        message: `Username must contain at least ${USERNAME_MIN_LENGTH} characters`,
+      })
+      .transform((value) => value.replaceAll(" ", "")),
+    password: z
+      .string()
+      .min(8, {
+        message: `Password must contain at least ${PASSWORD_MIN_LENGTH} characters`,
+      })
+      .max(20, {
+        message: `Password must contain at most ${PASSWORD_MAX_LENGTH} characters`,
+      }),
+    confirmPassword: z.string(),
+    firstName: z.string().default(""),
+    lastName: z.string().default(""),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+  });
