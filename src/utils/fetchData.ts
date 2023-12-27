cf11de4e06169559193842ae2d82c6a97366158c
Axel Asp <solidtop.asp@gmail.com>
2023-12-27 10:55:31 +0100

    Add fetch helper function

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,30 @@
+import HttpMethod from "@/enums/HttpMethod";
+
+async function fetchData<T>(
+  url: string,
+  method: HttpMethod,
+  data?: Object
+): Promise<T | null> {
+  try {
+    const init: RequestInit = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    if (data) {
+      init.body = JSON.stringify(data);
+    }
+
+    const res = await fetch(url, init);
+
+    const payload: T = await res.json();
+    return payload;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+export default fetchData;
