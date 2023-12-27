731f2e0dc20676c8ddaa4b7f50605f21022a9f90
Axel Asp <solidtop.asp@gmail.com>
2023-12-27 10:53:01 +0100

    Add client-side login page

--- src/app/login/content.tsx
+++ src/app/login/content.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import LoginForm from "@/components/login/LoginForm";
+import useUser from "@/hooks/useUser";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Content() {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user, router]);
+
   return (
     <main>
       <h1>Login</h1>
-      <LoginForm onLoginComplete={() => {}} />
+      <LoginForm
+        onLoginComplete={() => {
+          router.push("/");
+        }}
+      />
     </main>
   );
 }
