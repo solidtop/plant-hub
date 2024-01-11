b42dd127b5e616b1df5760f351a6a243b5983c7d
Axel Asp <solidtop.asp@gmail.com>
2024-01-11 13:20:01 +0100

    Wrap logo in anchor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
 import Image from "next/image";
 import Navbar from "./Navbar";
+import Link from "next/link";
 
 const Header: FC = () => {
   return (
     <header className="flex justify-between p-4 rounded-b-md bg-accent-color2 bg-opacity-30">
-      <Image src="/images/logo.png" width={50} height={50} alt="Logo" />
+      <Link href="/">
+        <Image src="/images/logo.png" width={50} height={50} alt="Logo" />
+      </Link>
       <Navbar />
     </header>
   );
